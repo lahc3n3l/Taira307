48a2011f8a023be2fda616f4b71521d573949d12
Mohammad Ghannam <154473723+ghannamX@users.noreply.github.com>
2024-10-10 23:50:04 +0200

    First PR to main (#1)
    * add Raspberry pico GPIOs image to readme
    
    * add Raspberry pico GPIOs image to readme
    
    ---------
    
    Co-authored-by: Mohammad Ghannam <mohammad.ghannam@sbg-systems.com>

--- Navigation/Scripts/Gps.py
+++ Navigation/Scripts/Gps.py
@@ -24,10 +24,8 @@ def getGPS(gpsModule):
         gpsModule.readline()
         buff = str(gpsModule.readline())
         parts = buff.split(',')
-        print(parts)
         if (parts[0] == "b'$GPGGA" and len(parts) == 15):
             if(parts[1] and parts[2] and parts[3] and parts[4] and parts[5] and parts[6] and parts[7]):
-                print(buff)
                 
                 latitude = convertToDegree(parts[2])
                 if (parts[3] == 'S'):
@@ -54,24 +52,42 @@ def convertToDegree(RawDegrees):
     Converted = float(firstdigits + nexttwodigits/60.0)
     Converted = '{0:.6f}'.format(Converted) 
     return str(Converted)
-    
-    
-while True:
+
+
+f = open("Positioning.txt", 'w')
+f.write("Logging data \n")
+n = 0
+while(n<600):
     
     getGPS(gpsModule)
 
     if(FIX_STATUS == True):
         print("Printing GPS data...")
-        print(" ")
-        print("Latitude: "+latitude)
-        print("Longitude: "+longitude)
-        print("Satellites: " +satellites)
-        print("Time: "+GPStime)
-        print("----------------------")
         
+        print("Latitude: "+latitude)
+        f.write("Latitude: "+latitude+ "\n")
+        
+        print("longitude: "+longitude)
+        f.write("longitude: "+longitude+ "\n")
+        
+        print("Satellites: " +satellites)
+        f.write("Satellites: "+satellites+ "\n")
+        
+        print("Time: "+GPStime)
+        f.write("Time: "+GPStime+ "\n")
+         i
+        print("----------------------")
+        f.write("--------------------\n")
         
         FIX_STATUS = False
+        
         
     if(TIMEOUT == True):
         print("No GPS data is found.")
         TIMEOUT = False
+        
+    n += 1
+        
+    
+f.close()
+    
