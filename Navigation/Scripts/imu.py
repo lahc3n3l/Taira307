d00c7716eb0eb1f6386db2ee104e8981044225cb
Mohammad Ghannam <mohammad.ghannam@sbg-systems.com>
2024-10-14 00:50:45 +0200

    add accelerometer config and calibration

--- Navigation/Scripts/imu.py
+++ Navigation/Scripts/imu.py
@@ -6,54 +6,75 @@ from ulab import numpy as np
 
 # Constants
 MPU6050_ADDR = 0x68
-#some MPU6050 Registers and their Address
-PWR_MGMT_1   = 0x6B
-CONFIG       = 0x1A
-GYRO_CONFIG  = 0x1B
-GYRO_XOUT_H  = 0x43
-GYRO_FS 	 = 2
-DLPF_CONFIG	 = 3
+# some MPU6050 Registers and their Address
+PWR_MGMT_1 = 0x6B
+CONFIG = 0x1A
+DLPF_CONFIG = 3
 
+GYRO_CONFIG = 0x1B
+GYRO_XOUT_H = 0x43
+GYRO_FS = 2
+
+ACCEL_CONFIG = 0x1C
+ACCEL_XOUT_H = 0x3B
+ACCEL_FS = 2
 # Initialize IMU I2C, 
 i2c = machine.I2C(0, sda=machine.Pin(0), scl=machine.Pin(1), freq=400000)
 
+
 def configureIMU():
-    # set power:
+    # 1. Set power:
     i2c.writeto_mem(MPU6050_ADDR, PWR_MGMT_1, b'\x00')
-    #Activate low-pass filter:
-    i2c.writeto_mem(MPU6050_ADDR, CONFIG, bytes([DLPF_CONFIG])) # good for airplane
-    #2. configure full scale range: 0: +-250 LSB/°/s, 1: +-500 LSB/°/s, 2: +-1000 LSB/°/s, 3: +2000 LSB/°/s
-    i2c.writeto_mem(MPU6050_ADDR, GYRO_CONFIG, bytes([GYRO_FS<<3]))
-    
-def readGyro():
-    #read from
-    gyroData = i2c.readfrom_mem(MPU6050_ADDR,GYRO_XOUT_H, 6)
+    # 2. Activate low-pass filter:
+    i2c.writeto_mem(MPU6050_ADDR, CONFIG, bytes([DLPF_CONFIG]))  # good for airplane
+    # 3. Configure full scale range: 0: +-250 LSB/°/s, 1: +-500 LSB/°/s, 2: +-1000 LSB/°/s, 3: +2000 LSB/°/s
+    i2c.writeto_mem(MPU6050_ADDR, GYRO_CONFIG, bytes([GYRO_FS << 3]))
+
+    # 4. Configure full scale range: 0: +-2g , 1: +-4g , 2: +-8g , 3: +-16g
+    i2c.writeto_mem(MPU6050_ADDR, ACCEL_CONFIG, bytes([ACCEL_FS << 3]))
+
+
+def readIMU():
+    # read from Gyro
+    gyroData = i2c.readfrom_mem(MPU6050_ADDR, GYRO_XOUT_H, 6)
     # Convert the data
     gx, gy, gz = struct.unpack('>hhh', gyroData)
-    scale = 131 /2**GYRO_FS
+    gScale = 131 / 2 ** GYRO_FS
     # convert to deg/s:
-    gx /= scale
-    gy /= scale
-    gz /= scale
-    return gx,gy,gz
+    gx /= gScale
+    gy /= gScale
+    gz /= gScale
 
-def calibrateGyro(n):
-    gyroBiases = np.array([0,0,0]) 
+    # read from Accel
+    accelData = i2c.readfrom_mem(MPU6050_ADDR, ACCEL_XOUT_H, 6)
+    # Convert the data
+    ax, ay, az = struct.unpack('>hhh', accelData)
+    aScale = 16384 / 2 ** ACCEL_FS
+    # convert to deg/s:
+    ax /= aScale
+    ay /= aScale
+    az /= aScale
+
+    return np.array([ax, ay, az]), np.array([gx, gy, gz])
+
+
+def calibrateIMU(n):
+    gyroBiases = np.array([0, 0, 0])
+    accelBiases = np.array([0, 0, 0])
     for i in range(n):
-        gx,gy,gz = readGyro()
-        gyroBiases += np.array([gx,gy,gz])
+        a, g = readIMU()
+        a[2] -= 1
+        gyroBiases += g
+        accelBiases += a
         utime.sleep_ms(1)
-    return gyroBiases / n
+    return accelBiases / n, gyroBiases / n
+
 
 # Main
-
 configureIMU()
-gyroBiases = calibrateGyro(2000)
-lastTime = utime.ticks_us
-theta = 0
+accelBiases, gyroBiases = calibrateIMU(2000)
 while True:
-    x, y, z = np.array(readGyro())- gyroBiases
-    dt = utime.ticks_us
-    theta += y *dt
-    print(x, y, z)
+    ax, ay, az = np.array(readIMU()[0]) - accelBiases
+    gx, gy, gz = np.array(readIMU()[1]) - gyroBiases
+    print(ax, ay, az)
     utime.sleep_ms(80)
