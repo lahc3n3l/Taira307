b12c983ba87a9c2eadac4aaf926b975254d55db8
ghannamX <ghannam.mohammad.tsi@gmail.com>
2024-10-20 21:39:47 +0200

    move Main section to init_py file

--- Navigation/Scripts/KalmanPitchRoll.py
+++ Navigation/Scripts/KalmanPitchRoll.py
@@ -109,24 +109,4 @@ class KalmanFilter:
         self.P = np.dot((np.eye(3) - np.dot(K, self.H)), self.P)
 
 
-# Main
-configureIMU()
-accelBiases,va, gyroBiases, vg = calibrateIMU(2000)
-kf = KalmanFilter()  # 80ms sampling rate
-lastTime = utime.ticks_us()
-print(accelBiases,va, gyroBiases, vg )
-while True:
-    ax, ay, az = np.array(readIMU()[0]) - accelBiases
-    gx, gy, gz = np.array(readIMU()[1]) - gyroBiases
-    
-    gyro = np.array([gx, gy, gz])
-    currentTime = utime.ticks_us()
-    dt = (currentTime - lastTime)/1e6
-    lastTime = currentTime
-    kf.predict(gyro, dt)
-    kf.update(np.array([ax, ay, az]))
 
-    roll, pitch, yaw = kf.x
-
-    print(f"Roll: {math.degrees(roll):.2f}°, Pitch: {math.degrees(pitch):.2f}°")#, Yaw: {math.degrees(yaw):.2f}°")
-    utime.sleep_ms(20)
