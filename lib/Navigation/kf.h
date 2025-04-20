8622d4a40aa83868966f4739b1e63e37e6dd0f6c
Mohammad Ghannam <ghannam.mohammad.tsi@gmail.com>
2025-04-20 06:25:10 +0200

    add eurler angles getters and initialize yaw with GNSS heading

--- lib/Navigation/kf.h
+++ lib/Navigation/kf.h
@@ -9,6 +9,14 @@ public:
     void predict(const BLA::Matrix<3>& u, float dt);
     void update(const BLA::Matrix<3>& accel);
     void getEulerAnglesDeg(float &roll, float &pitch, float &yaw) const;
+    float getRollRad() const { return x(0); }
+    float getPitchRad() const { return x(1); }
+    float getYawRad() const { return x(2); }
+
+    float getRollDeg() const { return x(0) * (float)RAD_TO_DEG; }
+    float getPitchDeg() const { return x(1) * (float)RAD_TO_DEG; }
+    float getYawDeg() const { return x(2) * (float)RAD_TO_DEG; }
+    void initializeYaw(float yaw) { x(2) = yaw; } // Initialize yaw with GNSS heading
 
 private:
     BLA::Matrix<3,3> A, B, Q, P;
