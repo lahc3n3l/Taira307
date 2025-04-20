3d7853ca906dc765d16be6891d9f7dd1e81c8f2b
Mohammad Ghannam <ghannam.mohammad.tsi@gmail.com>
2025-04-20 03:51:15 +0200

    cleanup KF and imu

--- src/main.cpp
+++ src/main.cpp
@@ -1,74 +1,60 @@
+// inclue the libraries
 #include <Arduino.h>
-// include wire.h for I2C communication
+#include <SparkFun_u-blox_GNSS_Arduino_Library.h>
 #include <Wire.h>
-// include IMU library
 #include <MPU6050.h>
 #include <kf.h>
+
+// define IMU instance
 MPU6050 imu;
 
-#define IMU_TEST
+// define GPS instance
+SFE_UBLOX_GNSS myGNSS;
 
-
+//define Kalman filter instance
 KalmanFilter kf;
+
+
 using namespace BLA;
 
 void setup() {
-
+ // begin the serial communication
   Serial.begin(115200); // USB serial for debug
   delay(1000);
-  #ifdef IMU_TEST
+  // begin the I2C communication
   Wire.begin();
-
-  imu.init(2, 250);  // 2g accel, 250 deg/s gyro*
-  Serial.println("Calibrating accelerometer...");
-  imu.calibrateAccel(); // Calibrate accelerometer
-  Serial.println("Calibrating gyroscope...");
-  imu.calibrateGyro(); // Calibrate gyroscope
-  float ax, ay, az;
-  imu.readAccel(ax, ay, az); // Read accelerometer data
-
-
-  delay(100);
-
-  
-    // ---------- End GPS Module Setup ----------
   }
 
 void loop() {
 
-  // ---------------- GY87 Sensor Test ----------------
-  #ifdef IMU_TEST
-
+  // define accelerometer and gyroscope variables
   float ax, ay, az;
   float gx, gy, gz;
-  
+  BLA::Matrix<3> eulerAngles = {0.0f, 0.0f, 0.0f}; // Initialize euler angles
+  // read the IMU data
   imu.readAccel(ax, ay, az); // Read accelerometer data
   imu.readGyro(gx, gy, gz); // Read gyroscope data
+  float dt = 0.01f; // Time step in seconds
+
+  
+  // define acceleration and gyroscope matrices
   BLA::Matrix<3, 1> accel = {ax, ay, az};
   BLA::Matrix<3, 1> gyro =   {gx, gy, gz}; 
-  gyro = gyro * (float) DEG_TO_RAD; // Convert to radians/s
 
+  // convert gyroscope data to radians/s
+  gyro = gyro * (float) DEG_TO_RAD; 
+
+  
+  //perform kalman filter prediction and update
   kf.predict(gyro, 0.01f); // Predict next state
   kf.update(accel); // Update Kalman filter with
-  float dt = 0.01f;
 
-
-
-delay(10);
-  float roll, pitch, yaw;
-  kf.getEulerAnglesDeg(roll, pitch, yaw); // Get Euler angles in degrees
-  Serial.print("Roll:");
-  Serial.print(roll);
-  Serial.print(" Pitch:");
-  Serial.print(pitch);
-  Serial.print(" Yaw:");
-  Serial.println(yaw);
+  // Get Euler angles in degrees
+  eulerAngles = kf.getEulerAnglesDeg(); 
   
-  #endif
-
+  delay(10);
 } // End of loop function
 
-#endif
 
 
 
