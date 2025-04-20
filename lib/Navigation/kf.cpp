ba42861913fa99a60fe9f7010527cca1e4855516
Mohammad Ghannam <ghannam.mohammad.tsi@gmail.com>
2025-04-20 02:29:28 +0200

    add pitch/roll Kalman Filter's context

---
+++ lib/Navigation/kf.cpp
@@ -0,0 +1,67 @@
+#include "kf.h"
+
+KalmanFilter::KalmanFilter() {
+    // A is identity as float
+    A = {1.0f,0.0f,0.0f,
+         0.0f,1.0f,0.0f,
+         0.0f,0.0f,1.0f};
+
+    // H maps roll and pitch from state x :float
+    // to measurement y :float
+    H = {1.0f,0.0f,0.0f,
+         0.0f,1.0f,0.0f};
+
+    // Initial covariance matrices
+    Q.Fill(0.0f);
+    R.Fill(0.0f);
+    P.Fill(0.0f);
+    x.Fill(0.0f);
+
+    // R ~ measurement noise
+    R(0,0) = R(1,1) = 0.05f * 0.05f;
+
+    // P ~ initial estimation error
+    P(0,0) = P(1,1) = P(2,2) = 5.0f * 5.0f;
+
+    // Initial Q will be filled in predict()
+    
+    Q(0,0) = Q(1,1) = Q(2,2) = 100.0f ;
+}
+
+void KalmanFilter::predict(const BLA::Matrix<3>& u, float dt) {
+    // B = dt * identity
+    B = {dt,0.0f,0.0f,
+         0.0f,dt,0.0f,
+         0.0f,0.0f,dt};
+
+    // Process noise Q = B * B^T * (1°)^2 in rad
+    // Q = dt^2 * identity
+    Q(0,0) = Q(1,1) = Q(2,2) = (dt * 30.0f * (float)DEG_TO_RAD ) * (dt * 30.0f * (float)DEG_TO_RAD ); // 1° in rad
+    x = A * x + B * u;
+    P = A * P * ~A + Q;
+}
+
+void KalmanFilter::update(const BLA::Matrix<3>& accel) {
+    float ax = accel(0);
+    float ay = accel(1);
+    float az = accel(2);
+
+    BLA::Matrix<2> y;
+    y(0) = atan2f(ay, sqrtf(ax*ax + az*az));     // roll
+    y(1) = atan2f(-ax, sqrtf(ay*ay + az*az));    // pitch
+
+    BLA::Matrix<2,2> S = H * P * ~H + R;
+    bool invertible =  Invert(S);
+    BLA::Matrix<3,2> K = P * ~H * S;
+
+    x = x + K* (y - H * x);
+    BLA::Matrix<3,3> I = BLA::Matrix<3,3>{1.0f,0.0f,0.0f, 0.0f,1.0f,0.0f, 0.0f,0.0f,1.0f};
+    // P = (I - K * H) * P;
+    P = (I - K * H) * P;
+}
+
+void KalmanFilter::getEulerAnglesDeg(float& roll, float& pitch, float& yaw) {
+    roll  = x(0) * RAD_TO_DEG;
+    pitch = x(1) * RAD_TO_DEG;
+    yaw   = x(2) * RAD_TO_DEG;
+}
---
+++ lib/Navigation/kf.h
@@ -0,0 +1,18 @@
+#include <BasicLinearAlgebra.h>
+#include <math.h>
+
+
+class KalmanFilter {
+public:
+    KalmanFilter();
+
+    void predict(const BLA::Matrix<3>& u, float dt);
+    void update(const BLA::Matrix<3>& accel);
+    void getEulerAnglesDeg(float& roll, float& pitch, float& yaw);
+
+private:
+    BLA::Matrix<3,3> A, B, Q, P;
+    BLA::Matrix<2,3> H;
+    BLA::Matrix<2,2> R;
+    BLA::Matrix<3> x;
+};
