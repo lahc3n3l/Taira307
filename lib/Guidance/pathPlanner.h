2d7a22f55f3ab0866bac3c5a30b3db0a1d79e326
Mohammad Ghannam <ghannam.mohammad.tsi@gmail.com>
2025-04-27 15:29:51 +0200

    pathPlanner : define pathPlanner class in header file

--- lib/Guidance/pathPlanner.h
+++ lib/Guidance/pathPlanner.h
@@ -26,12 +26,40 @@ struct fullState {
     position pos;
     bool isPosValid; // true if position is valid
     bool isAttValid; // true if attitude is valid
-
-    bool isAttValid; // true if attitude is valid
     attitude att;
+    bool isAttValid; // true if attitude is valid
+
     float groundSpeed; // in m/s
     bool isGroundSpeedValid; // true if ground speed is valid
     //TODO: add airspeed, vertical speed, etc.
 };
 
+class pathPlanner {
+
+    public:
+
+        // default constructor
+        pathPlanner();
+
+        // destructor
+        ~pathPlanner();
+
+        // function to add a waypoint to the list of waypoints
+        void addWaypoint(float lon, float lat, float altitude, float speed);
+
+        void setMission(std::vector<waypoint> waypoints);
+
+        waypoint getNextWaypoint();
+
+        void updateCurrentState(fullState state);
+
+
+    private:
+        std::vector<waypoint> waypoints; // list of waypoints
+        fullState currentState; // current state of the vehicle
+        int currentWaypointIdx = 0; // index to track the current waypoint
+        float arrivalThd = 1.0 ; // threshold to consider the waypoint as reached (in meters)
+
+        
+};
 #endif // PATH_PLANNER_H
