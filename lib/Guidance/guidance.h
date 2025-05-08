0a449fadd3681f3a85bab23350d1521e10e43ac3
Mohammad Ghannam <ghannam.mohammad.tsi@gmail.com>
2025-05-08 16:32:23 +0200

    guidance : init Guidance block

---
+++ lib/Guidance/guidance.h
@@ -0,0 +1,17 @@
+#ifndef GUIDANCE_H
+#define GUIDANCE_H
+
+
+//
+// THIS FILE IS PART OF THE GUIDANCE LIBRARY.
+//
+
+class guidance {
+    public:
+        // default constructor
+        guidance();
+        
+        
+};
+
+#endif // GUIDANCE_H
